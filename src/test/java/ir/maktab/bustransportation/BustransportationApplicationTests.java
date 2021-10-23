45db8603a1d0a501a043e65582c0378d07dae2d5
A-Khoobi <87467061+a-khoobi@users.noreply.github.com>
2021-10-23 10:38:27 +0330

    start

---
+++ src/test/java/ir/maktab/bustransportation/BustransportationApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.maktab.bustransportation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BustransportationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
